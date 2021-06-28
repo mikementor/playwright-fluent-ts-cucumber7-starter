cc7b5196bf49ed10c2bcad5bfbb86ffa860c8507
Марат Чукмаров <mchukmarov@infotech.team>
2021-06-28 17:33:28 +0300

    filter

--- stories/common/report-page-errors.stories.ts
+++ stories/common/report-page-errors.stories.ts
@@ -1,7 +1,7 @@
 import { CustomWorld } from '../../world';
 import { StoryWithProps } from 'playwright-fluent';
 
-const stackTraceErrorsToIgnore = ['carbon.js', 'rate limit exceeded'];
+const stackTraceErrorsToIgnore = ['carbonads.net', 'carbon.js', 'rate limit exceeded'];
 function shouldIgnoreErrorByStacktrace(pageError: Error): boolean {
   if (pageError.stack) {
     return stackTraceErrorsToIgnore.some((fragment) => pageError.stack?.includes(fragment));
